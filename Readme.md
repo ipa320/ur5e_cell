a53905cbfd5b237a3022b30911db42126aa80718
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2022-08-05 16:30:05 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Robot workcell description
+This package provides the workcell description of the ur5e robot, Shunk egp50 gripper, intel realsense D435 camera and the custom workcell. 
+
+
+### To check xacro syntax
+````
+check_urdf <(xacro workcell.urdf.xacro)
+````
---
+++ workcell_description/launch/workcell_bringup.py
@@ -0,0 +1,50 @@
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    robot_urdf = os.path.join(get_package_share_directory('workcell_description'), 'urdf', 'workcell.urdf.xacro')
+    robot_urdf = xacro.process_file(robot_urdf)
+    robot_description = {'robot_description': robot_urdf.toxml()}
+
+    rviz_config_file = os.path.join(get_package_share_directory('workcell_description'), 'launch', 'config.rviz')
+
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_urdf",
+            description="URDF file",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "rviz_config_file",
+            description="rviz configuration file",
+        )
+    )
+
+    robot_state_pub_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        arguments=[robot_urdf],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    nodes_to_start=[
+        robot_state_pub_node,
+        rviz_node
+    ]
+
+    return launch.LaunchDescription(declared_arguments+nodes_to_start)
+
