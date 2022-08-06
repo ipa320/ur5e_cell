98b180a528b669a6cf5cb4ecd7bfd52b8beb6eb8
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2022-08-06 13:06:43 +0200

    fixed issues with urdf files

--- workcell_description/launch/workcell_bringup.launch.py
+++ workcell_description/launch/workcell_bringup.launch.py
@@ -3,24 +3,18 @@ import os
 import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
     robot_urdf = os.path.join(get_package_share_directory('workcell_description'), 'urdf', 'workcell.urdf.xacro')
     robot_urdf = xacro.process_file(robot_urdf)
     robot_description = {'robot_description': robot_urdf.toxml()}
-
-    rviz_config_file = os.path.join(get_package_share_directory('workcell_description'), 'config', 'config.rviz')
-
+    rviz_config = os.path.join(get_package_share_directory('workcell_description'), 'config', 'config.rviz')
     declared_arguments = []
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "robot_urdf",
-            description="URDF file",
-        )
-    )
+
     declared_arguments.append(
         DeclareLaunchArgument(
             "rviz_config_file",
@@ -32,20 +26,32 @@ def generate_launch_description():
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        arguments=[robot_urdf],
+        parameters=[
+            {"use_sim_time": use_sim_time}, 
+            {"robot_description": robot_description},
+        ],
     )
     rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='log',
+        arguments=['-d', str(rviz_config)],
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen',
+        name='joint_state_publisher_gui',
+        parameters=[{"use_sim_time": use_sim_time},],
     )
 
     nodes_to_start=[
         robot_state_pub_node,
-        rviz_node
+        rviz_node,
+        joint_state_publisher_gui_node,
     ]
 
-    return launch.LaunchDescription(declared_arguments+nodes_to_start)
+    return LaunchDescription(declared_arguments+nodes_to_start)
 
