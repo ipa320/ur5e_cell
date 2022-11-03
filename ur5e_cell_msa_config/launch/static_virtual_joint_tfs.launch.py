380ec23d7867446737fdad638b4873ff16d70a0c
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2022-11-03 14:12:36 +0100

    added Docker support

--- ur5e_cell_msa_config/launch/static_virtual_joint_tfs.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ur5e_workcell", package_name="ur5e_cell_msa_config").to_moveit_configs()
-    return generate_static_virtual_joint_tfs_launch(moveit_config)
